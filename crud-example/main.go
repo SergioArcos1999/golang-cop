424197a92e9fd9dfbcf3e45cdca4131b0b09ae5e
Sergio Arcos <sergio.arcos@mango.com>
2022-05-18 21:03:30 +0200

    Simple REST CRUD

--- crud-example/main.go
+++ crud-example/main.go
@@ -1,15 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func sum(a int, b int) int {
-	return a + b
+type Books []Book
+
+type Book struct {
+	Id    string `json:"id"`
+	Title string `json:"title"`
+	Pages int    `json:"pages"`
 }
 
-func main() {
-	a := 2
-	b := 2
+type GetAllBooksResponse struct {
+	BookList Books `json:"books"`
+	Quantity int   `json:"quantity"`
+}
 
-	result := sum(a, b)
-	fmt.Printf("Result is: %d\n", result)
+var bookList Books
+
+func main() {
+	http.HandleFunc("/books", handleRead)
+	http.HandleFunc("/add-book", handleWrite)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handleWrite(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "POST":
+		decoder := json.NewDecoder(request.Body)
+		var book Book
+		decoder.Decode(&book)
+
+		bookList = append(bookList, book)
+		fmt.Println(bookList)
+	default:
+		message := request.Method + " method not allowed"
+		http.Error(writer, message, http.StatusMethodNotAllowed)
+	}
+}
+
+func handleRead(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		response := GetAllBooksResponse{bookList, len(bookList)}
+		json.NewEncoder(writer).Encode(response)
+	default:
+		message := request.Method + " method not allowed"
+		http.Error(writer, message, http.StatusMethodNotAllowed)
+	}
 }
